1d852bf6dc108f4e270f2d0a2548f3757e726d77
adityabarodiya <adi25kht@gmail.com>
2024-02-14 11:40:38 +0530

    update

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Starting')
+var { graphql, buildSchema } = require("graphql");
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var rootValue = { hello: () => "Hello world!" };
+
+var source = "{ hello }";
+
+graphql({ schema, source, rootValue }).then((response) => {
+  console.log(response);
+});
